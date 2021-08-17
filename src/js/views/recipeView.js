fd5e669239864bd3ae3aa21e796ad4d8d4ec3554
HoangZoan <d.hoang1906@gmail.com>
2021-08-17 15:49:52 +0700

    New Feature

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,4 +1,5 @@
 import icons from 'url:../../img/icons.svg';
+import { Fraction } from 'fractional';
 
 class RecipeView {
   _data;
@@ -110,9 +111,7 @@ class RecipeView {
         <use href="${icons}#icon-check"></use>
       </svg>
       <div class="recipe__quantity">${
-        ing.quantity
-        /* ? new Fraction(ing.quantity).toString() : ''
-         */
+        ing.quantity ? new Fraction(ing.quantity).toString() : ''
       }</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
