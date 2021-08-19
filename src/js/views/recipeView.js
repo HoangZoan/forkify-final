ea287014f13dd7905206e7824b821dca8db5ed75
HoangZoan <d.hoang1906@gmail.com>
2021-08-19 16:36:38 +0700

    Update feature

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,6 @@
 import icons from 'url:../../img/icons.svg';
 import { View } from './view';
+import { formatQuantity } from '../helpers';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -119,7 +120,7 @@ class RecipeView extends View {
         <use href="${icons}#icon-check"></use>
       </svg>
       <div class="recipe__quantity">${
-        ing.quantity === null ? '' : ing.quantity
+        ing.quantity === null ? '' : formatQuantity(ing.quantity)
       }</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
