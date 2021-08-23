e61bbad0ae511d2e513d57ac560abb4ea5446973
HoangZoan <d.hoang1906@gmail.com>
2021-08-23 23:41:28 +0700

    Complete validation feature for add recipe

--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -6,6 +6,7 @@ class AddRecipeView extends View {
   _windowEl = document.querySelector('.add-recipe-window');
   _formEl = document.querySelector('.upload');
   _submitAvailable = true;
+  _valueTypeValidation = [];
 
   constructor() {
     super();
@@ -23,7 +24,7 @@ class AddRecipeView extends View {
         if (!this._submitAvailable) return;
         const formData = [...new FormData(this._formEl)];
         const data = Object.fromEntries(formData);
-        console.log(data);
+        // console.log(data);
       }.bind(this)
     );
   }
@@ -77,26 +78,52 @@ class AddRecipeView extends View {
         const span = this.closest('span');
         const input3 = span.querySelector('input[data-type="Description"]');
 
+        // Check value type of 'Quantity' field, if value contains not a number type
+        // then send message and stop submit handler
+        if (el.dataset.type === 'Quantity') {
+          const validateResult = _this._validateFirst2Field(
+            input1Elements,
+            'quantity'
+          );
+          _this._setUploadAvailable(validateResult, 'quantity');
+        }
+
         // Check value type of 'Unit' field, if value contains number
         // then send message and stop submit handler
         if (el.dataset.type === 'Unit') {
-          const validateResult = _this._validateUnitField(el);
-          if (validateResult) _this._submitAvailable = true;
-          else _this._submitAvailable = false;
+          const validateResult = _this._validateFirst2Field(
+            input2Elements,
+            'unit'
+          );
+          _this._setUploadAvailable(validateResult, 'unit');
         }
 
         // Check if 'Description' value required to be put in
+        // If there are values, set 'required' to the 'Description' input
         if (this.value !== '') {
           input3.setAttribute('required', 'true');
-        } else {
-          ing1DescInput.setAttribute('required', 'true');
+
+          // If the first 2 inputs of ingredients 1 have value, then skip to
+          // remain the 'required'
+          if (span.dataset.first) return;
+
+          // Remove the 'required' from 'Description' input in 1st ingredient
+          ing1DescInput.removeAttribute('required');
         }
 
-        if (span.dataset.first) return;
-
-        if (this.value !== '') {
-          ing1DescInput.removeAttribute('required');
-        } else {
+        // If there are NO value, remain 'required' of the first ingredient 'Description' input
+        else {
+          const emptyValueCheck = input3Elements.some(el => el.value !== '');
+          // If there are NO value in both the first 2 inputs of the first ingredient,
+          // then remain 'required' of the 'Description' input
+          if (
+            ing1QuantityInput.value === '' &&
+            ing1UnitInput.value === '' &&
+            !emptyValueCheck
+          ) {
+            ing1DescInput.setAttribute('required', 'true');
+            return;
+          }
           input3.removeAttribute('required');
         }
       })
@@ -105,31 +132,66 @@ class AddRecipeView extends View {
     // Validate 'Description' inputs
     input3Elements.forEach(el =>
       el.addEventListener('blur', function () {
-        const span = this.closest('span');
+        const emptyValueCheck = input3Elements.some(el => el.value !== '');
 
-        if (span.dataset.first) return;
+        // If there are values in the "Description" inputs
+        if (emptyValueCheck) {
+          // If there are value in any of the first 2 inputs of the first ingredient,
+          // then remain 'required' of the 'Description' input
+          if (ing1QuantityInput.value !== '' || ing1UnitInput.value !== '') {
+            ing1DescInput.setAttribute('required', 'true');
+            return;
+          }
 
-        if (this.value !== '')
-          ing1QuantityInput.value === '' &&
-            ing1UnitInput.value === '' &&
-            ing1DescInput.removeAttribute('required');
-        else ing1DescInput.setAttribute('required', 'true');
+          // If there are NO values of the first 2 inputs of the first ingredient,
+          // then remove the 'required' of the 'Description' input
+          ing1DescInput.removeAttribute('required');
+        }
+        // If there are values in the "Description" inputs
+        else {
+          ing1DescInput.setAttribute('required', 'true');
+        }
       })
     );
   }
 
-  _validateUnitField(unitEl) {
-    const messageEl = document.querySelector('.warning-sign--unit');
-    const valueArr = unitEl.value.split('');
-    const numberTypeCheck = valueArr.some(val => Number.isFinite(+val));
+  _validateFirst2Field(inputsArr, type) {
+    const messageEl = document.querySelector(`.warning-sign--${type}`);
+    const newInputArr = inputsArr.flatMap(input => input.value.split(''));
+    let numberCheck;
 
-    // Show message if the value includes number
-    if (numberTypeCheck) messageEl.classList.remove('hidden');
-    if (unitEl.value === '' || !numberTypeCheck)
+    if (newInputArr.length === 0) {
       messageEl.classList.add('hidden');
+      return true;
+    }
 
-    if (unitEl.value === '') return true;
-    return !numberTypeCheck;
+    // Check if input values contain number. If true, show message
+    if (type === 'quantity')
+      numberCheck = newInputArr.every(
+        letter => Number.isFinite(+letter) == true
+      );
+
+    if (type === 'unit') {
+      numberCheck = !newInputArr.some(
+        letter => Number.isFinite(+letter) == true
+      );
+    }
+
+    if (!numberCheck) messageEl.classList.remove('hidden');
+    else messageEl.classList.add('hidden');
+
+    // // Return a Boolean to determine the upload availability
+    return numberCheck;
+  }
+
+  _setUploadAvailable(result, type) {
+    if (type === 'quantity') this._valueTypeValidation[0] = result;
+    if (type === 'unit') this._valueTypeValidation[1] = result;
+
+    const resultCheck = this._valueTypeValidation.some(res => res == false);
+
+    if (!resultCheck) this._submitAvailable = true;
+    else this._submitAvailable = false;
   }
 }
 
